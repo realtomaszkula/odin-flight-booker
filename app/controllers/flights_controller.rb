aeec01f5f1444fdac1084011467028e65b1ae275
Tomasz Kula <tomaszkula07@gmail.com>
2016-03-01 00:58:34 +0100

    Add strftime format for search result

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,12 +4,26 @@ class FlightsController < ApplicationController
     @num = [[1,1],[2,2],[3,3],[4,4]]
     possible_dates = Flight.where(start_id: 1).order(:departure_date).uniq.pluck(:departure_date)
     @dates = format_and_map(possible_dates)
+
+    @search_query = search_query?
+    search if search_query?
   end
+
+  private
 
   def format_and_map(possible_dates)
     possible_dates.map! { |date| [date.strftime("%d/%m/%Y"), date.strftime("%d%m%Y")] }
   end
 
+  def search_query?
+    (params.keys & ['from_airport', 'to_airport', 'num_tickets', 'date']).any?
+  end
+
+  def search
+    @results = Flight.where('start_id = :start AND finish_id = :finish',
+                      start: params[:from_airport],
+                      finish: params[:to_airport])
+  end
 
 
 end
