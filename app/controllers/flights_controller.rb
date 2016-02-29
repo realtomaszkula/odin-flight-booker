24806c3d374d1116c861c8a66e6750adce709ab2
Tomasz Kula <tomaszkula07@gmail.com>
2016-03-01 00:21:22 +0100

    Bootstrap style radio btn

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,12 +4,10 @@ class FlightsController < ApplicationController
     @num = [[1,1],[2,2],[3,3],[4,4]]
     possible_dates = Flight.where(start_id: 1).order(:departure_date).uniq.pluck(:departure_date)
     @dates = format_and_map(possible_dates)
-
-
   end
 
   def format_and_map(possible_dates)
-    possible_dates.map! { |date| [date.strftime("%d/%m/%Y"), date.strftime("%d/%m/%Y")] }
+    possible_dates.map! { |date| [date.strftime("%d/%m/%Y"), date.strftime("%d%m%Y")] }
   end
 
 
