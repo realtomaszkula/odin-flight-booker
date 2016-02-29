8f3e97648765a50d4717d7d904b520d212373884
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-29 23:55:46 +0100

    Finish populating dropdowns

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,15 @@
 class FlightsController < ApplicationController
   def index
+    @airports = Airport.all.map { |airport| [ airport.code, airport.id ] }
+    @num = [[1,1],[2,2],[3,3],[4,4]]
+    possible_dates = Flight.where(start_id: 1).order(:departure_date).uniq.pluck(:departure_date)
+    @dates = format_and_map(possible_dates)
   end
+
+  def format_and_map(possible_dates)
+    possible_dates.map! { |date| [date.strftime("%d/%m/%Y"), date.strftime("%d/%m/%Y")] }
+  end
+
+
+
 end
