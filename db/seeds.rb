534a905c095c4f99dea53cfd8788ba21a4ca62fb
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-29 22:50:06 +0100

    Seed 200 flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,18 @@ Airport.create( code: 'ATL')
 Airport.create( code: 'LAX')
 Airport.create( code: 'PEK')
 Airport.create( code: 'FRA')
+
+
+200.times do
+  airports = [1,2,3,4,5]
+
+  airport_from = airports.sample
+  airport_to = airports.sample
+  flight_duration = [60, 90, 120, 240, 360].sample
+  date = Faker::Date.forward(30)
+
+  Flight.create(start_id: airport_from,
+                finish_id: airport_to,
+                  departure_date: date,
+                    flight_duration: flight_duration )
+end
