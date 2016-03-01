9641c78f18621c66af34651e67324d626a135543
Tomasz Kula <tomaszkula07@gmail.com>
2016-03-01 12:42:22 +0100

    Proper Booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,18 +2,20 @@ class BookingsController < ApplicationController
 
   def show
     @booking = Booking.find(params[:id])
+    @flight = @booking.flight
+    @passengers = @booking.passengers
   end
 
   def new
     @flight = Flight.find_by(id: params[:booking][:flight_id])
     @booking = Booking.new
-    @passengers = build_passengers
+    @passengers = build_passengers_new
   end
 
   def create
     @booking = Booking.new
     @booking.flight_id = params[:booking][:flight_id]
-    @flight = @booking.flight
+    build_passengers_create
 
     if @booking.save
       redirect_to @booking
@@ -24,7 +26,16 @@ class BookingsController < ApplicationController
 
   private
 
-  def build_passengers
+  def build_passengers_create
+    params[:booking][:passengers_attributes].each do | _k, value|
+      @booking.passengers.build(
+        name:   value["name"],
+        email:  value["email"]
+        )
+    end
+  end
+
+  def build_passengers_new
     num_of_tickets = params[:booking][:num_of_tickets].to_i
     num_of_tickets.times { @booking.passengers.build }
     @booking.passengers
@@ -32,7 +43,7 @@ class BookingsController < ApplicationController
 
   def bookings_params
     params.require(:booking)
-          .permit(:flight_id, passengers_attributes: [:id, :name, :email])
+          .permit(:flight_id, passengers_attributes: [:name, :email])
   end
 
 end
