7d764155c01c2b165379c17168e2c57890d96e00
Tomasz Kula <tomaszkula07@gmail.com>
2016-03-01 13:24:31 +0100

    Finished

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,7 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all.map { |airport| [ airport.code, airport.id ] }
     @num = [[1,1],[2,2],[3,3],[4,4]]
-    possible_dates = Flight.where(start_id: 1).order(:departure_date).uniq.pluck(:departure_date)
+    possible_dates = Flight.order(:departure_date).uniq.pluck(:departure_date)
     @dates = format_and_map(possible_dates)
 
     @search_query = search_query?
